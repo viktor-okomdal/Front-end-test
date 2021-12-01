e8a8a1fd5af1fb55b4021ce895eddc28d7aa78c3
Viktor Okomdal <viktor_okomdal@hotmail.com>
2021-12-01 14:13:02 +0100

    db and test

---
+++ src/tests/component-grid-test.tsx
@@ -0,0 +1,11 @@
+import {icecream} from "../database/database"
+import {
+  getByLabelText,
+  getByText,
+  getByTestId,
+  queryByTestId,
+  waitFor,
+} from '@testing-library/dom'
+import '@testing-library/jest-dom'
+
+
